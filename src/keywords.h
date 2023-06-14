549907ac642d55548aa79ff033a72dbb586a09a7
Yao Zi <ziyao@disroot.org>
2023-06-14 15:26:16 +0800

    Add langauge-specific keywords
    - Golang
    - Elm
    - rclang

--- src/keywords.h
+++ src/keywords.h
@@ -37,6 +37,30 @@ wchar_t *__cppKeywords[] = {
 	NULL
 };
 
+wchar_t *__rclangKeywords[] = {
+	L"val", L"sal", L"s8", L"u8", L"s16", L"u16", L"s32", L"u32",
+	L"s64", L"u64", L"for", L"fn", L"if", L"else", L"dcl", L"ret",
+	L"break", L"export", L"szo", L"ptr",
+	NULL
+};
+
+wchar_t *__golangKeywords[] = {
+	L"go", L"func", L"int", L"uint", L"byte", L"int8", L"int16", L"int32",
+	L"int64", L"uint8", L"uint16", L"uint32", L"uint64", L"string", L"map",
+	L"make", L"len", L"struct", L"type", L"import", L"return", L"package",
+	L"if", L"for", L"else", L"select", L"switch", L"chan", L"bool", L"true",
+	L"false", L"continue", L"break", L"nil", L"case", L"var",
+	NULL
+};
+
+wchar_t *__elmKeywords[] = {
+	L"module", L"Cmd", L"Sub", L"List", L"type", L"alias", L"case", L"of",
+	L"import", L"module", L"exposing", L"as", L"String", L"Int", L"if",
+	L"then", L"else", L"Maybe", L"Result", L"Just", L"Nothing", L"True",
+	L"False", L"Err", L"Ok", L"Bool",
+	NULL
+};
+
 /*
  *	K[] must be defined as an array, which contains (char*, wchar**) pairs,
  *	representing the suffix of a type of file and keywords used in the
@@ -50,5 +74,8 @@ Keyword_Class K[] = {
 	{ ".sh", __shellKeywords },
 	{ ".cpp", __cppKeywords },
 	{ ".hpp", __cppKeywords },
+	{ ".rcs", __rclangKeywords },
+	{ ".go", __golangKeywords },
+	{ ".elm", __elmKeywords },
 	{ NULL, NULL },
 };
